82a90808d63699697129db788a1d5b40094c48a6
danewheaton <wheaton.dane@gmail.com>
2017-08-21 11:29:02 -0500

    teleportation mechanic

---
+++ Faces/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField] Transform destination;
+    
+    Vector3 playerLocalPosition;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.SetParent(transform);
+        playerLocalPosition = other.transform.localPosition;
+        other.transform.SetParent(destination);
+        other.transform.localPosition = playerLocalPosition;
+    }
+}
