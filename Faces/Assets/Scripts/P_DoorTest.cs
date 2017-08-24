faeab9b6651522a1dcd0da0599ff4619b465ebad
danewheaton <wheaton.dane@gmail.com>
2017-08-24 18:22:46 -0500

    test script for doors

---
+++ Faces/Assets/Scripts/P_DoorTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P_DoorTest : MonoBehaviour
+{
+    [SerializeField] GameObject[] doors;
+
+    GameObject nextDoor;
+    int counter;
+
+    private void Start()
+    {
+        nextDoor = doors[0];
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && counter < doors.Length)
+        {
+            nextDoor.SetActive(false);
+            counter++;
+            nextDoor = doors[counter];
+        }
+    }
+}
